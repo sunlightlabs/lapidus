1601f253209efac0ba3d3f13c74414cb1cc52bb8
Daniel Cloud <dcloud@sunlightfoundation.com>
2011-11-14 13:24:34 -0500

    Added a percentage filter to dashboard_filters. Moved the render observation template code into a 'snippet' since we are using the same complex if...else sequence in more than one place. Made that snippet able to handle ratios that are not time based or percentages...

--- dashboard/templatetags/dashboard_filters.py
+++ dashboard/templatetags/dashboard_filters.py
@@ -7,7 +7,17 @@ import datetime
 @stringfilter
 def secondstoduration(value):
     """formats a value in seconds to a time format"""
-    td = datetime.timedelta(seconds=round(float(value)))
-    return str(td)
+    # td = datetime.timedelta(seconds=round(float(value)))
+    hours, remainder = divmod(int(round(float(value))), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    if not hours:
+        return "{minutes}m{seconds:02}s".format(minutes=minutes, seconds=seconds)
+    return "{hours}h{minutes}m{seconds:02}s".format(hours=hours,minutes=minutes, seconds=seconds)
 
 register.filter('secondstoduration', secondstoduration)
+
+def percentage(value, arg=0):
+    """converts a ratio (float) value to a string representing percentage"""
+    return "{value:.{precision}%}".format(value=value, precision=int(arg))
+
+register.filter('percentage', percentage)
