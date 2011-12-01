5348d9628f0d892816dd378c8d73526d8ba960e2
Daniel Cloud <dcloud@sunlightfoundation.com>
2011-12-01 15:13:08 -0500

    Fixed API and did some testing. Didn't implement creation of ratioobservations via API, but you can add new CountObservations and ListObservations.

--- settings.py
+++ settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    # 'tastypie',
+    'tastypie',
     'django_extensions',
     'lapidus.metrics',
     'lapidus.dashboard',
--- urls.py
+++ urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-# from lapidus.metrics.resources import ProjectResource, AnnotationResource, MetricDetailResource, ObservationResource
-# from tastypie.api import Api
+from lapidus.metrics.resources import ProjectResource, AnnotationResource, MetricDetailResource, MetricResource, ObservationResource
+from tastypie.api import Api
 
 admin.autodiscover()
 
-# api = Api(api_name='api')
-# api.register(ProjectResource())
-# api.register(AnnotationResource())
-# api.register(MetricDetailResource())
-# api.register(ObservationResource())
+api = Api(api_name='api')
+api.register(ProjectResource())
+api.register(AnnotationResource())
+api.register(MetricDetailResource())
+api.register(ObservationResource())
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('dashboard.urls')),
-    # url(r'^', include(api.urls)),
+    url(r'^', include(api.urls)),
 )
