b2239f62609fd30b311f149906dfa4d9d348d87d
Daniel Cloud <dcloud@sunlightfoundation.com>
2011-12-06 15:39:00 -0500

    Further changes to support display of per-project data.

--- site_static/js/script.js
+++ site_static/js/script.js
@@ -24,18 +24,4 @@
         console.log("From " + fromDate + " to " + toDate);
         if (fromDate > toDate) dfFrom.datepicker("setDate", toDate);
     });
-    $('#analytics').tablesorter({
-        sortList: [
-            [0, 0]
-        ],
-        cssChildRow: "secondary",
-        widgets: ["zebra"]//,
-    });
-
-    $('.secondary').hide();
-    $('#analytics').delegate('tbody tr.primary th', 'click', function() {
-        $(this).closest('tr').toggleClass('expanded').nextUntil('tr:not(.secondary)').toggle();
-        return false;
-    });
-
 }(jQuery));
