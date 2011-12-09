a839c178d1ee31b577fb2fce05232f508c061bb0
Daniel Cloud <dcloud@sunlightfoundation.com>
2011-12-09 11:36:47 -0500

    Fix to make the link to a project page clickable in the th element that normally performs a jQuery toggle of elements.

--- site_static/js/sorttable.js
+++ site_static/js/sorttable.js
@@ -6,10 +6,14 @@
         cssChildRow: "secondary",
         widgets: ["zebra"]//,
     });
-
+    // Hide secondary rows and set up toggle event on th
     $('.secondary').hide();
-    $('#analytics').delegate('tbody tr.primary th', 'click', function() {
+    $('#analytics').on('click', 'tbody tr.primary th', function() {
         $(this).closest('tr').toggleClass('expanded').nextUntil('tr:not(.secondary)').toggle();
         return false;
+    });
+    // Prevent anchors in th from propogating event so default can occur rather than toggle event.
+    $('#analytics').on('click', 'tbody tr.primary a', function() {
+        event.stopPropagation();
     });
 }());
